1eff359280e4e615f19c129b57d1ecf384c224a2
Chaemin Yooun <cinnamein@gmail.com>
2025-05-15 13:12:42 +0900

    ♻️Refactor: jwt 반환 방식 변경
    - header에서 cookie로 변경

--- src/main/java/oridungjeol/duckhang/security/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/oridungjeol/duckhang/security/oauth2/OAuth2SuccessHandler.java
@@ -1,9 +1,11 @@
 package oridungjeol.duckhang.security.oauth2;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
+    @Value("${jwt.access-token-validity-in-milliseconds}")
+    private long accessTokenValidityInMilliseconds;
+
+    @Value("${jwt.refresh-token-validity-in-milliseconds}")
+    private long refreshTokenValidityInMilliseconds;
+
     private final AuthJpaRepository authJpaRepository;
     private final JwtGenerator jwtGenerator;
 
@@ -27,8 +35,20 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
             Authentication authentication
     ) throws IOException, ServletException {
         CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
         String accessToken = jwtGenerator.createAccessToken(customOAuth2User);
+        Cookie accessCookie = new Cookie("accessToken", accessToken);
+        accessCookie.setHttpOnly(true);
+        accessCookie.setSecure(true);
+        accessCookie.setPath("/");
+        accessCookie.setMaxAge((int) accessTokenValidityInMilliseconds / 1000);
+
         String refreshToken = jwtGenerator.createRefreshToken(customOAuth2User);
+        Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(true);
+        refreshCookie.setPath("/");
+        refreshCookie.setMaxAge((int) refreshTokenValidityInMilliseconds / 1000);
 
         String providerId = customOAuth2User.getProviderId();
         Auth auth = authJpaRepository.findByProviderId(providerId)
@@ -37,7 +57,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         auth.setRefreshToken(refreshToken);
         authJpaRepository.save(auth);
 
-        response.addHeader("Authorization", "Bearer " + accessToken);
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
         response.sendRedirect("/user/profile");
     }
 }
