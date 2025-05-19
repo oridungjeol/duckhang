e6f7d117ad2791c217502e2085fea08d794cc72c
Azamman327 <wjsdbdud327@naver.com>
2025-05-19 11:36:39 +0900

    🐛Fix: LatLng 찾지 못하던 오류 수정

--- src/main/java/oridungjeol/duckhang/map/application/dto/LatLng.java
+++ src/main/java/oridungjeol/duckhang/map/application/dto/LatLng.java
@@ -1,12 +1,12 @@
 package oridungjeol.duckhang.map.application.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LatLng {
     private double latitude;
     private double longitude;
