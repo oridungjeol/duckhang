490a2c14f85bf6ce46ede3eefe32c3ba90790ee2
Azamman327 <wjsdbdud327@naver.com>
2025-06-02 14:41:49 +0900

    ChatDocument Document 테이블명 변경

--- src/main/java/oridungjeol/duckhang/chat/infrastructure/elasticsearch/document/ChatDocument.java
+++ src/main/java/oridungjeol/duckhang/chat/infrastructure/elasticsearch/document/ChatDocument.java
@@ -20,7 +20,7 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(indexName="chatting_dataset")
+@Document(indexName="chatting")
 public class ChatDocument {
     @Id
     private String id;
